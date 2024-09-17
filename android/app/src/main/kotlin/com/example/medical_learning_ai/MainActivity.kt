d08c83c4774e421b46b6f153f33653dc7deadd23
Wataru Kato <vv01sam@gmail.com>
2024-09-17 11:34:02 +0900

    現在の状態をコミット

---
+++ android/app/src/main/kotlin/com/example/medical_learning_ai/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.medical_learning_ai
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Firebase Initialization',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ホームページ'),
+      ),
+      body: Center(
+        child: Text('Firebaseが正常に初期化されました！'),
+      ),
+    );
+  }
+}
